e90fa01d8cec6676b9fb3f7e9dbb8d51deb321fc
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2020-12-25 21:45:56 +0100

    Update script.js

--- hw-3/script.js
+++ hw-3/script.js
@@ -21,3 +21,44 @@ var questionInterval;
 
 
 init();
+
+function init() {
+  clearDetails();
+  reset();
+  
+  let heading = document.createElement("p");
+  heading.setAttribute("id", "main-heading");
+  heading.textContent = "Proverite svoje znanje!";
+
+  
+  let instructions = document.createElement("p");
+  instructions.setAttribute("id", "instructions");
+  instructions.textContent = " Dobrodošli u JavaScript kviz iz predmeta Programiranje internet aplikacija. Pred vama je 10 pitanja, pri čemu birate da li odgovarate na pitanje iz opšte kulture ili iz informacionih tehnologija."; 
+
+  // startJsQuiz
+  let startOpstaPitanja = document.createElement("button");
+  startOpstaPitanja.setAttribute("id", "startOpstaPitanja");
+  startOpstaPitanja.setAttribute("class", "btn btn-secondary");
+  startOpstaPitanja.textContent= "Kviz opšteg znanja";
+
+  
+  let startItPitanja = document.createElement("button");
+  startItPitanja.setAttribute("id", "startItPitanja");
+  startItPitanja.setAttribute("class", "btn btn-secondary");
+  startItPitanja.textContent= "Kviz IT znanja";
+
+  mainEl.appendChild(heading);
+  mainEl.appendChild(instructions);
+  mainEl.appendChild(startOpstaPitanja);
+  mainEl.appendChild(startItPitanja);
+
+  startOpstaPitanja.addEventListener("click", function () {
+    quizType = "Kviz opšteg znanja";
+    playQuiz(opstaPitanja);
+  });
+
+  startItPitanja.addEventListener("click", function () {
+    quizType = "Informacione tehnologije";
+    playQuiz(itPitanja);
+  });
+}
