a0f1b0a8da4ecef2d8b17335eba11c42a3041b0d
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2020-12-25 21:38:21 +0100

    Update questions.js
    unos opstih pitanja kviza

--- hw-3/questions.js
+++ hw-3/questions.js
@@ -1,6 +1,55 @@
 var opstaPitanja = [
-  
-  ];
+  {
+    title: "Prvi Srpski ustanak počeo je gde i koje godine?",
+    choices: ["Orašac, 1804.", "Takovo, 1815.", "Kragujevac, 1839.", "Košutnjak, 1868."],
+    answer: "Orašac, 1804."
+  },
+  {
+    title: "Ćelija je...?",
+    choices: ["osnovna jedinica građe i funkcije svih živih bića, sem virusa.", "mali infektivni agens.", "zbir morfoloških i biohemijskih reakcija.", "hronična, funkcionalna, vaskularna glavobolja."],
+    answer: "osnovna jedinica građe i funkcije svih živih bića, sem virusa."
+  },
+  {
+    title: "Koje godine je Novak Đoković osvojio svoj prvi Australian Open",
+    choices: ["2014.", "2007.", "2008.", "2012."],
+    answer: "2008."
+  },
+  {
+    title: "Tenk je prvi put upotrebljen u kojoj bici u Prvom Svetskom ratu i koje godine?",
+    choices: ["Kolubarska bitka, 1914.", "Bitka na Somi, 1918.", "Bitka na Somi, 1915.", "Bitka na Somi, 1916. godine."],
+    answer: "Bitka na Somi, 1916. godine."
+  },
+  {
+    title: "Koje godine je doneta Deklaracija nezavinosti SAD-a?",
+    choices: ["1783.", "1801.", "1776.", "1902."],
+    answer: "1776."
+  },
+  {
+    title: "Ko je režirao film Vuk sa Vol strita",
+    choices: ["Martin Skorseze", "Stiven Spilberg", "Gaj Riči", "Kventin Tarantino"],
+    answer: "Martin Skorseze"
+  },
+  {
+    title: "Ko glumi mladog Vita Korleonea u trilogiji Kum?",
+    choices: ["Bred Pit.", "Robert de Niro.", "Al Paćino.", "Leonardo DiKaprio."],
+    answer: "Robert de Niro."
+  },
+{
+    title: "Glavni grad Turske je...",
+    choices: ["Istanbul.", "Kušadasi.", "Ankara.", "Antalija."],
+    answer: "Ankara."
+  },
+{
+    title: "Ivo Andrić dobio je Nobelovu nagradu za književnost za delo...",
+    choices: ["Kuća na Osami.", "Omer-paša Latas.", "Gospođica.", "Na Drini ćuprija."],
+    answer: "Na Drini ćuprija."
+  },
+  {
+    title: "Delo Braća Karamazovi napisao je ruski pisac...",
+    choices: ["Lav Nikolajevič Tolstoj", "Fjodor M. Dostojevski", "Gogolj", "Aleksandar Sergejevič Puškin"],
+    answer: "Fjodor M. Dostojevski"
+  }
+];
 
 var itPitanja = [
   ];
