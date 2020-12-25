1e6edf0dc232b5131e6984cf8a707401157a9de0
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2020-12-25 21:43:46 +0100

    Create script.js

---
+++ hw-3/script.js
@@ -0,0 +1,23 @@
+let highscoreDiv = document.querySelector("#highscore");
+let gameTimerEl = document.querySelector("#gameTimer");
+let quesTimerEl = document.querySelector("#quesTimer");
+let mainEl = document.querySelector("#details");
+let timerTab = document.querySelector("#timers");
+
+
+
+var test = false;
+var score = 0;
+var quiz = {};
+var quizType = "";
+
+var gameDuration = 0;
+var gameSecElapsed = 0;
+var gameInterval;
+
+var questionDuration = 20;
+var questionSecElapsed = 0;
+var questionInterval;
+
+
+init();
