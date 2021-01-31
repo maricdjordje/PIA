5b3f83444497e187c02176de050c34acdfcf48af
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2021-01-31 23:46:24 +0100

    Update konekcija.php

--- hw-4/kontroler/konekcija.php
+++ hw-4/kontroler/konekcija.php
@@ -1,1 +1,9 @@
+<?php
+$conn = new mysqli('localhost','root','','korisnici');
 
+if ($conn->connect_error) {
+   die("Greška: Došlo je do neke greške!".$conn->connect_error);
+} else {
+    echo "Uspešno povezivanje sa bazom!";
+}
+?>
