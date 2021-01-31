38c2eb98d7302ecb2e957fbfe1052f87001bed8f
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2021-01-31 23:45:37 +0100

    Update izbrisi-korisnika.php

--- hw-4/kontroler/izbrisi-korisnika.php
+++ hw-4/kontroler/izbrisi-korisnika.php
@@ -1,1 +1,15 @@
+<?php
 
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('location: ../index.php#popup1');
+}
+
+include('connectdb.php');
+$id=$_REQUEST['id'];
+
+$del = "DELETE  FROM korisnici WHERE korisnikID= '$id'"; 
+$result = $conn -> query($del);
+header("Location: ../administrator/users.php"); 
+$conn->close();
+?>
