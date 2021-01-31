037b7ba8d3da0442ef0b2d9695672405bf8d6919
maricdjordje <51753850+maricdjordje@users.noreply.github.com>
2021-01-31 23:15:54 +0100

    Update index.php

--- hw-4/index.php
+++ hw-4/index.php
@@ -1,1 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('location: index.php');
+}
+elseif (isset($_SESSION['username']) == "admin") {
+    header('location: administrator/index.php');
+}
+else{ 
+    header('location: korisnik/index.php');
+}
 
+
+?>
